a3d79e37656788348f3fae4a5c3d5e6c26f2cbcb
steph <g00367573@atu.ie>
2023-12-21 19:48:06 +0000

    Initial commit

---
+++ src/main/java/ie/atu/hello/GameTrackerMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameTrackerMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameTrackerMiniProjectApplication.class, args);
+	}
+
+}
